38e036f995ed937a0c034ffbeb4973e57bd62824
Komugisama <xiaomaimai1996@gmail.com>
2023-10-18 20:18:26 +0800

    Added subfolder traversal and regular expression support

--- barcode_reader.py
+++ barcode_reader.py
@@ -1,76 +1,109 @@
-'''
-Author: chentx
-Date: 2020-11-06 16:28:35
-LastEditTime: 2022-04-20 14:10:28
-LastEditors: chentx
-Description: 植物标本照片条码识别工具，使用ZBAR解码（不支持PE等标本馆使用的CODABAR编码）
-'''
-import os, time
-import tkinter as tk
+import os
+import time
 from tkinter import filedialog
-import pyzbar.pyzbar as pyzbar
-from PIL import Image
-from PIL import ImageFile
+import tkinter as tk
+from PIL import Image, ImageFile
+from pyzbar.pyzbar import decode, ZBarSymbol
+import re
+from tqdm import tqdm
+
+# 设置Image的配置
 Image.MAX_IMAGE_PIXELS = 240000000
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-def getBarcode(path):
-    # try:
-    img = Image.open(path)
-    # except BaseException as err:
-    #     return False, 'can not open image:'.format(err)
-    barcodeInfoList = pyzbar.decode(img)
-    if barcodeInfoList != []:
-        barcodeInfo = barcodeInfoList[0]
-        barcode = barcodeInfo.data.decode('utf-8').replace(' ','')
-        return True, barcode
-    else:
-        return False, 'barcode not found'
 
-print("植物标本照片条码识别工具 Beta 0.0.5")
-print("作者：陈天翔 chentx@ibcas.ac.cn 2020-11-19\n")
+def get_barcode(path, allowed_symbols, user_regex):
+    try:
+        img = Image.open(path)
+        barcode_info_list = decode(img, symbols=allowed_symbols)
+        if barcode_info_list:
+            barcodes = [barcode_info.data.decode(
+                "utf-8").replace(" ", "") for barcode_info in barcode_info_list]
+            if user_regex:
+                pattern = re.compile(user_regex)
+                matched_barcodes = [
+                    barcode for barcode in barcodes if pattern.match(barcode)]
+                if matched_barcodes:
+                    return True, matched_barcodes[0]
+            return True, barcodes[0]
+        else:
+            return False, "barcode not found"
+    except Exception as err:
+        return False, f"can not open image: {err}"
 
-print("选择待处理文件夹")
-root = tk.Tk()
-root.withdraw()
 
-path = filedialog.askdirectory()
-print("已选路径：", path)
+def process_images_in_folder(folder_path, user_regex):
+    allowed_extensions = {".jpg", ".jpeg", ".png", ".gif", ".tif", ".tiff"}
+    allowed_symbols = [ZBarSymbol.CODE128, ZBarSymbol.CODE39,
+                       ZBarSymbol.CODE93, ZBarSymbol.CODABAR]
 
-successCount = 0
-failedCount = 0
+    success_count = 0
+    failed_count = 0
+    file_count = 0
 
-log = open(os.path.join(path, 'log.txt'), 'a', encoding='utf-8')
-log.write('selected folder: ' + path + '\n')
-log.write('start at ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '\n\n')
-for root, directorys, files in os.walk(path):
-    for file in files:
-        allowedExtension = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG', '.gif', '.GIF']
-        extension = os.path.splitext(file)[1]
-        if extension in allowedExtension:
-            imagePath = os.path.join(root, file)
-            (status, info) = getBarcode(imagePath)
-            if status:
-                try:
-                    os.rename(imagePath, os.path.join(root, info + extension))
-                except Exception as e:
-                    msg = file + ' : ' + str(e)
-                    print(msg)
-                    log.write(msg + '\n')
-                    failedCount += 1
-                else:
-                    msg = file + ' : ' + info + extension
-                    print(msg)
-                    log.write(msg + '\n')
-                    successCount += 1
-            else:
-                msg = file + ' : ' + info
-                print(msg)
-                log.write(msg + '\n')
-                failedCount += 1
+    log_path = os.path.join(folder_path, "log.txt")
 
-log.write('\nfinished at ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ', ' + str(successCount) + ' success, ' + str(failedCount) + ' failed' + '\n\n\n\n\n')
-log.close()
+    with open(log_path, "a", encoding="utf-8") as log:
+        log.write(f"selected folder: {folder_path}\n")
+        log.write(
+            f"start at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n\n")
 
-print ('\nfinished at ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ', ' + str(successCount) + ' success, ' + str(failedCount) + ' failed')
-input('按任意键退出')
+        for root, directories, files in os.walk(folder_path):
+            for file in files:
+                extension = os.path.splitext(file)[1]
+                if extension.lower() in allowed_extensions:
+                    file_count += 1
+
+        with tqdm(total=file_count, ascii=True) as pbar:
+            for root, directories, files in os.walk(folder_path):
+                for file in files:
+                    extension = os.path.splitext(file)[1]
+                    if extension.lower() in allowed_extensions:
+                        image_path = os.path.join(root, file)
+                        status, info = get_barcode(
+                            image_path, allowed_symbols, user_regex)
+                        if status:
+                            try:
+                                new_filename = info + extension
+                                os.rename(image_path, os.path.join(
+                                    root, new_filename))
+                                log_msg = f"{file} : {new_filename}"
+                                log.write(log_msg + "\n")
+                                success_count += 1
+                            except Exception as e:
+                                log_msg = f"{file} : {str(e)}"
+                                log.write(log_msg + "\n")
+                                failed_count += 1
+                        else:
+                            log_msg = f"{file} : {info}"
+                            log.write(log_msg + "\n")
+                            failed_count += 1
+                        pbar.update(1)
+
+        log.write(
+            f"\nfinished at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}, "
+            f"{success_count} success, {failed_count} failed\n\n\n\n\n"
+        )
+
+    print(
+        f"\nfinished at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}, "
+        f"{success_count} success, {failed_count} failed"
+    )
+
+
+if __name__ == "__main__":
+    print("植物标本照片条码重命名工具 Beta 0.1.1")
+    print("2023-10-18 国家植物标本资源库 chentx@ibcas.ac.cn\n")
+
+    print("选择待处理文件夹")
+    root = tk.Tk()
+    root.withdraw()
+
+    path = filedialog.askdirectory()
+    print("已选路径：", path)
+
+    user_regex = input("输入正则表达式 (用于精确匹配条形码，不使用正则请按Enter键跳过)：")
+
+    process_images_in_folder(path, user_regex)
+
+    input("按任意键退出")
--- codabar_reader.py
+++ codabar_reader.py
@@ -1,10 +1,10 @@
-'''
+"""
 Author: chentx
 Date: 2020-11-06 16:28:35
-LastEditTime: 2020-11-19 17:40:58
+LastEditTime: 2022-11-02 17:04:35
 LastEditors: chentx
 Description: 植物标本照片条码识别工具，使用ZXING解码（支持PE等标本馆使用的CODABAR编码）
-'''
+"""
 import os, time
 import tkinter as tk
 from tkinter import filedialog
@@ -25,12 +25,21 @@ reader = BarCodeReader()
 successCount = 0
 failedCount = 0
 
-log = open(os.path.join(path, 'log.txt'), 'a')
-log.write('selected folder: ' + path + '\n')
-log.write('start at ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '\n\n')
+log = open(os.path.join(path, "log.txt"), "a")
+log.write("selected folder: " + path + "\n")
+log.write("start at " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "\n\n")
 for root, directorys, files in os.walk(path):
     for file in files:
-        allowedExtension = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG', '.gif', '.GIF']
+        allowedExtension = [
+            ".jpg",
+            ".JPG",
+            ".jpeg",
+            ".JPEG",
+            ".png",
+            ".PNG",
+            ".gif",
+            ".GIF",
+        ]
         extension = os.path.splitext(file)[1]
         if extension in allowedExtension:
             imagePath = os.path.join(root, file)
@@ -38,32 +47,51 @@ for root, directorys, files in os.walk(path):
                 result = reader.decode(imagePath, True)
                 decoded = result[0]
             except Exception as e:
-                msg = file + ' : ' + str(e)
+                msg = file + " : " + str(e)
                 print(msg)
-                log.write(msg + '\n')
+                log.write(msg + "\n")
                 failedCount += 1
             else:
-                if decoded.get('parsed'): 
+                if decoded.get("parsed"):
                     try:
-                        os.rename(imagePath, os.path.join(root, decoded['parsed'] + extension))
+                        os.rename(
+                            imagePath, os.path.join(root, decoded["parsed"] + extension)
+                        )
                     except Exception as e:
-                        msg = file + ' : ' + str(e)
+                        msg = file + " : " + str(e)
                         print(msg)
-                        log.write(msg + '\n')
+                        log.write(msg + "\n")
                         failedCount += 1
                     else:
-                        msg = file + ' : ' + decoded['parsed'] + extension
+                        msg = file + " : " + decoded["parsed"] + extension
                         print(msg)
-                        log.write(msg + '\n')
+                        log.write(msg + "\n")
                         successCount += 1
                 else:
-                    msg = file + ' : barcode not found'
+                    msg = file + " : barcode not found"
                     print(msg)
-                    log.write(msg + '\n')
+                    log.write(msg + "\n")
                     failedCount += 1
 
-log.write('\nfinished at ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ', ' + str(successCount) + ' success, ' + str(failedCount) + ' failed' + '\n\n\n\n\n')
+log.write(
+    "\nfinished at "
+    + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    + ", "
+    + str(successCount)
+    + " success, "
+    + str(failedCount)
+    + " failed"
+    + "\n\n\n\n\n"
+)
 log.close()
 
-print ('\nfinished at ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ', ' + str(successCount) + ' success, ' + str(failedCount) + ' failed')
-input('press any button to exit')
+print(
+    "\nfinished at "
+    + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    + ", "
+    + str(successCount)
+    + " success, "
+    + str(failedCount)
+    + " failed"
+)
+input("press any button to exit")
